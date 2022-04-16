0b3c8678320bbce4976d83471a7d6726a3b35e46
Chongyi Zheng <harry@harryzheng.com>
2022-04-16 16:43:05 -0400

    Add list command

---
+++ cmd/list.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/harryzcy/mailbox-cli/internal/email"
+	"github.com/spf13/cobra"
+)
+
+// listCmd represents the list command
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List emails",
+	Run: func(cmd *cobra.Command, args []string) {
+		client := email.Client{
+			APIID:  cmd.Flag("api-id").Value.String(),
+			Region: cmd.Flag("region").Value.String(),
+		}
+		result, err := client.List(email.ListOptions{
+			Type:       cmd.Flag("type").Value.String(),
+			Year:       cmd.Flag("year").Value.String(),
+			Month:      cmd.Flag("month").Value.String(),
+			Order:      cmd.Flag("order").Value.String(),
+			NextCursor: cmd.Flag("next-cursor").Value.String(),
+		})
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println(result)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+	listCmd.Flags().String("api-id", "", "API ID")
+	listCmd.Flags().String("region", "", "Region")
+	listCmd.Flags().String("type", "", "Type")
+	listCmd.Flags().String("year", "", "Year")
+	listCmd.Flags().String("month", "", "Month")
+	listCmd.Flags().String("order", "", "Order")
+	listCmd.Flags().String("next-cursor", "", "Next Cursor")
+}
