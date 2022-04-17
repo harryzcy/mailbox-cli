c56cde0ac48d0289c63c2cb22a61726c259f4231
Chongyi Zheng <harry@harryzheng.com>
2022-04-16 23:39:48 -0400

    Set messageID as argument

--- cmd/get.go
+++ cmd/get.go
@@ -13,12 +13,18 @@ var getCmd = &cobra.Command{
 	Use:   "get",
 	Short: "Get an email by messageID",
 	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) != 1 {
+			fmt.Println("Please specify a messageID")
+			os.Exit(1)
+		}
+		messageID := args[0]
+
 		client := email.Client{
 			APIID:  cmd.Flag("api-id").Value.String(),
 			Region: cmd.Flag("region").Value.String(),
 		}
 		result, err := client.Get(email.GetOptions{
-			MessageID: cmd.Flag("message-id").Value.String(),
+			MessageID: messageID,
 		})
 		if err != nil {
 			fmt.Println(err)
@@ -30,5 +36,4 @@ var getCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(getCmd)
-	getCmd.Flags().String("message-id", "", "MessageID")
 }
